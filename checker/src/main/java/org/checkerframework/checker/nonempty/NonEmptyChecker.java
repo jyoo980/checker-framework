90d8795dac76c131aa7c5010d05bf69392392222
James Yoo <jyoo980@gmail.com>
2024-02-22 16:56:31 -0800

    Make `DelegationChecker` a subchecker of `NonEmptyChecker`

--- checker/src/main/java/org/checkerframework/checker/nonempty/NonEmptyChecker.java
+++ checker/src/main/java/org/checkerframework/checker/nonempty/NonEmptyChecker.java
@@ -1,9 +1,19 @@
 package org.checkerframework.checker.nonempty;
 
+import java.util.Set;
+import org.checkerframework.common.basetype.BaseTypeChecker;
+
 /**
  * A type-checker that prevents {@link java.util.NoSuchElementException} in the use of container
  * classes.
  *
  * @checker_framework.manual #non-empty-checker Non-Empty Checker
  */
-public class NonEmptyChecker extends DelegationChecker {}
+public class NonEmptyChecker extends BaseTypeChecker {
+  @Override
+  protected Set<Class<? extends BaseTypeChecker>> getImmediateSubcheckerClasses() {
+    Set<Class<? extends BaseTypeChecker>> subcheckers = super.getImmediateSubcheckerClasses();
+    subcheckers.add(DelegationChecker.class);
+    return subcheckers;
+  }
+}
